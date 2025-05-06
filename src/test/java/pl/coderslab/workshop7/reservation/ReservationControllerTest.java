3763a08eaa095121d63b45fb6001c585716c497f
Uliana-Pylypenko <upylypen@visa.com>
2025-05-06 16:08:39 +0200

    controller - tests for createReseravtion

--- src/test/java/pl/coderslab/workshop7/reservation/ReservationControllerTest.java
+++ src/test/java/pl/coderslab/workshop7/reservation/ReservationControllerTest.java
@@ -1,6 +1,7 @@
 package pl.coderslab.workshop7.reservation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -8,6 +9,8 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import javax.swing.text.html.parser.Entity;
+import java.time.DateTimeException;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -51,7 +54,61 @@ class ReservationControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.reservationStart").value(String.valueOf(reservationStart)))
                 .andExpect(jsonPath("$.reservationEnd").value(String.valueOf(reservationEnd)));
+    }
 
+    @Test
+    void createReservationCatchDateTimeParseException() throws Exception {
+        Long userId = 1L;
+        Long accommodationId = 1L;
+        String reservationStart = "20.04.2024";
+        String reservationEnd = "20.05.2025";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/reservation/create")
+                .param("userId", String.valueOf(userId))
+                .param("accommodationId", String.valueOf(accommodationId))
+                .param("reservationStartString", reservationStart)
+                .param("reservationEndString", reservationEnd))
+
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createReservationCatchIllegalArgumentException() throws Exception {
+        Long userId = 1L;
+        Long accommodationId = 1L;
+        LocalDate reservationStart = LocalDate.of(2025, 1, 1);
+        LocalDate reservationEnd = LocalDate.of(2020, 1, 1);
+
+        when(reservationService.create(userId, accommodationId, reservationStart, reservationEnd)).thenThrow(IllegalArgumentException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/reservation/create")
+                        .param("userId", String.valueOf(userId))
+                        .param("accommodationId", String.valueOf(accommodationId))
+                        .param("reservationStartString", String.valueOf(reservationStart))
+                        .param("reservationEndString", String.valueOf(reservationEnd)))
+
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createReservationCatchEntityNotFoundException() throws Exception {
+        Long userId = 1L;
+        Long accommodationId = 1L;
+        LocalDate reservationStart = LocalDate.of(2025, 1, 1);
+        LocalDate reservationEnd = LocalDate.of(2020, 1, 1);
+
+        when(reservationService.create(userId, accommodationId, reservationStart, reservationEnd)).thenThrow(EntityNotFoundException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/reservation/create")
+                        .param("userId", String.valueOf(userId))
+                        .param("accommodationId", String.valueOf(accommodationId))
+                        .param("reservationStartString", String.valueOf(reservationStart))
+                        .param("reservationEndString", String.valueOf(reservationEnd)))
+
+                .andDo(print())
+                .andExpect(status().isNotFound());
     }
 
 }
